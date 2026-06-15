92a2b3b896075dfe6ab4513552a0d03d5cb3a24a
Shriniket Yakkundi <56179616+p1nkm7n@users.noreply.github.com>
2026-06-15 14:28:56 +0530

    Add: top-k-elements-in-list - submission-8

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-8.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        let countMap = new Map();
+
+        for (const num of nums){
+            let currentCount = countMap.get(num) || 0;
+            countMap.set(num, currentCount + 1);
+        }
+
+        let sortedMap = [...countMap].sort((a, b) => b[1] - a[1]);
+        return sortedMap.slice(0, k).map(([num]) => num);
+    }
+}
