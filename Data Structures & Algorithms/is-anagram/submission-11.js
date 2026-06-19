2b8876e5d25116b8f248266e1172ece550047d26
Shriniket Yakkundi <56179616+p1nkm7n@users.noreply.github.com>
2026-06-19 18:20:25 +0530

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length != t.length){
+            return false;
+        }
+        
+        let map = new Map();
+
+        for (const ch of s){
+            let charCount = map.get(ch) || 0;
+            map.set(ch, charCount + 1);
+        }
+
+        for (const ch of t){
+            if (!map.has(ch)){
+                return false;
+            }
+
+            let charCount = map.get(ch);
+            if (charCount == 1){
+                map.delete(ch);
+            }
+            else {
+                map.set(ch, charCount - 1);
+            }
+        }
+
+        return map.size == 0;
+    }
+}
