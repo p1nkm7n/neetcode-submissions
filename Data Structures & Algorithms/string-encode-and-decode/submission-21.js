a1d8199434aee0aa32510c81bfca8685a3f632ee
Shriniket Yakkundi <56179616+p1nkm7n@users.noreply.github.com>
2026-07-13 15:20:56 +0530

    Add: string-encode-and-decode - submission-21

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-21.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let encodedString = "";
+
+        for (const str of strs){
+            encodedString += str.length + "|" + str;
+        }
+        return encodedString;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        let result = [];
+        let i = 0;
+        while (i < str.length){
+            let j = i;
+            while (str[j] != "|"){
+                j++;
+            }
+            let length = Number(str.slice(i, j));
+            let start = j + 1;
+            let end = start + length;
+            result.push(str.slice(start, end));
+            i = end;
+        }
+        return result;
+    }
+}
