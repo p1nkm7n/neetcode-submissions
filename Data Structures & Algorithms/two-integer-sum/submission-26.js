78c6a1034df1198a31c6de8fa6cfcb0afb32e3d9
Shriniket Yakkundi <56179616+p1nkm7n@users.noreply.github.com>
2026-09-07 11:09:25 +0530

    Add: two-integer-sum - submission-26

---
+++ Data Structures & Algorithms/two-integer-sum/submission-26.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    // twoSum(nums, target) {
+    //     for (let i = 0; i < nums.length; i++){
+    //         for (let j = i + 1; j < nums.length; j++){
+    //             if (nums[i] + nums[j] == target){
+    //                 return [i, j];
+    //             }
+    //         }
+    //     }
+    // }
+
+    twoSum(nums, target) {
+        let map = new Map();
+        for (let i = 0; i < nums.length; i++){
+            let complement = target - nums[i];
+            if (map.has(complement)){
+                return [map.get(complement),i];
+            }
+            map.set(nums[i], i);
+        }
+        return [];
+    }
+
+}
